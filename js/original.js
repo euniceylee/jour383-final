dacfd156fb254a4b180668bef28f4341a2b9eea9
euniceylee <euniceyl@u.northwestern.edu>
2016-03-12 16:23:00 -0600

    pre chaotic isotope

--- js/original.js
+++ js/original.js
@@ -1,2 +1,25 @@
 // $("#flipcard").flip();
 
+// // init Isotope
+// var $grid = $('.grid').isotope({
+//   // options
+// });
+// // filter items on button click
+// $('.filter-button-group').on( 'click', 'button', function() {
+//   var filterValue = $(this).attr('data-filter');
+//   $container.isotope({ filter: filterValue });
+// });
+//   // show if name ends with -ium
+//   nosegrouponly: function() {
+//     var nose = $(this).find('.nose').text();
+//     return name.match( /nosegrouponly$/ );
+//   }
+// };
+
+// // filter items on button click
+// $('.filter-button-group').on( 'click', 'button', function() {
+//   var filterValue = $(this).attr('data-filter');
+//   // use filter function if value matches
+//   filterValue = filterFns[ filterValue ] || filterValue;
+//   $grid.isotope({ filter: filterValue });
+// });
