f431df8c60f31b72a94b1009ae3f03b46acb6a15
euniceylee <euniceyl@u.northwestern.edu>
2016-03-13 00:42:17 -0600

    fixed images, text boxes, and introduction

--- js/original.js
+++ js/original.js
@@ -1,25 +1,43 @@
 // $("#flipcard").flip();
 
-// // init Isotope
-// var $grid = $('.grid').isotope({
-//   // options
-// });
-// // filter items on button click
-// $('.filter-button-group').on( 'click', 'button', function() {
-//   var filterValue = $(this).attr('data-filter');
-//   $container.isotope({ filter: filterValue });
-// });
-//   // show if name ends with -ium
-//   nosegrouponly: function() {
-//     var nose = $(this).find('.nose').text();
-//     return name.match( /nosegrouponly$/ );
-//   }
-// };
+$( function() {
+  // init Isotope
+  var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'masonry'
+  });
+  
+  // If you only wanna filter nose then use the call below
+  // $grid.isotope({ filter: '.nose' })
 
-// // filter items on button click
-// $('.filter-button-group').on( 'click', 'button', function() {
-//   var filterValue = $(this).attr('data-filter');
-//   // use filter function if value matches
-//   filterValue = filterFns[ filterValue ] || filterValue;
-//   $grid.isotope({ filter: filterValue });
-// });
+  // filter functions
+  var filterFns = {
+    // // show if number is greater than 50
+    // numberGreaterThan50: function() {
+    //   var number = $(this).find('.number').text();
+    //   return parseInt( number, 10 ) > 50;
+    // },
+    // show if name ends with -ium
+    nosegroup: function() {
+      var name = $(this).find('.nose').text();
+      return name.match( /nosegroup$/ );
+    }
+  };
+
+  // bind filter button click
+  $('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+  
+});
